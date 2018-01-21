ded8e54be915ebf97fd4df83ec3e067d049dc159
Alex Bush <bushnoh@gmail.com>
2018-01-21 17:42:49 +0000

    Problem 13

--- src/main/scala/level1/Problem7.scala
+++ src/main/scala/level1/Problem7.scala
@@ -4,14 +4,15 @@ object Problem7 {
 
   def primeStream: Stream[Int] = {
 
-    def next(prev: List[Int]): Stream[Int] = {
+    def next(prev: Vector[Int]): Stream[Int] = {
       val n = Stream.from(prev.last + 2).filter { i =>
-        !prev.exists(i % _ == 0)
+        val rti = math.sqrt(i).floor.toInt
+        !prev.takeWhile(_ <= rti).exists(i % _ == 0)
       }.head
       Stream.cons(n, next(prev.:+(n)))
     }
 
-    Stream.cons(2, Stream.cons(3, next(List(2, 3))))
+    Stream.cons(2, Stream.cons(3, next(Vector(2, 3))))
 
   }
 
