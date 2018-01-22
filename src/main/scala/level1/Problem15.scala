3da9359e989d4840998b250584574e3eb60cf916
Alex Bush <bushnoh@gmail.com>
2018-01-22 17:44:02 +0000

    Problem 15

--- src/main/scala/level1/Problem15.scala
+++ src/main/scala/level1/Problem15.scala
@@ -2,26 +2,12 @@ package level1
 
 object Problem15 {
 
-  def findNumRoutesSlow(dim: Int): Int = {
+  def findNumRoutes(dim: Int): Long = {
 
-    def loop(stack: List[(Int, Int)], acc: Int): Int = stack match {
-      case Nil => acc
-      case (`dim`, `dim`) :: t => loop(t, acc + 1)
-      case (x, `dim`) :: t => loop((x + 1, dim) :: t, acc)
-      case (`dim`, y) :: t => loop((dim, y + 1) :: t, acc)
-      case (x, y) :: t => loop((x + 1, y) :: (x, y + 1) :: t, acc)
-    }
-
-    loop(List((0, 0)), 0)
-
-  }
-
-  def findNumRoutes(dim: Int): Int = {
-
-    def loop(stack: Vector[(Int, Int)], score: Map[(Int, Int), Int]): Int = {
+    def loop(stack: Vector[(Int, Int)], score: Map[(Int, Int), Long]): Long = {
       stack match {
         case (0, 0) +: t => score((0, 1)) + score((1, 0))
-        case (x, y) +: t if x < 0 || y < 0 => loop(t, score)
+        case (x, y) +: t if x < 0 || y < 0 || score.contains((x, y)) => loop(t, score)
         case (`dim`, y) +: t =>
           val newStack = t :+ (dim - 1, y) :+ (dim, y - 1)
           val newScore = score.+((dim -> y, score(dim, y + 1)))
@@ -40,13 +26,13 @@ object Problem15 {
       }
     }
 
-    loop(Vector((dim, dim - 1), (dim - 1, dim)), Map(((dim, dim), 1)))
+    loop(Vector((dim, dim - 1), (dim - 1, dim)), Map(((dim, dim), 1L)))
 
   }
 
 
   def main(args: Array[String]): Unit = {
-    println(s"Answer: ${findNumRoutes(15)}")
+    println(s"Answer: ${findNumRoutes(20)}")
   }
 
 }
