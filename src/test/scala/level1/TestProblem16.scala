bd0958158bcc22c9444aaffa268433223cc5187d
Alex Bush <alex.bush@coxauto.co.uk>
2018-01-26 17:08:03 +0000

    Problem 16

---
+++ src/test/scala/level1/TestProblem16.scala
@@ -0,0 +1,16 @@
+package level1
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestProblem16 extends FlatSpec with Matchers {
+
+
+  "BigInt.*" should "multiply a number by 3" in {
+    (Problem16.BigInt(Seq(9, 9, 9)) * 3).toString should be("2997")
+  }
+
+  "powerSum" should "return the example" in {
+    Problem16.powerSum(15) should be(26)
+  }
+
+}
