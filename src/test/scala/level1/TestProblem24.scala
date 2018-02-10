5999198d7975e02431bce523c919adba3039a11d
Alex Bush <bushnoh@gmail.com>
2018-02-10 13:50:17 +0000

    Problem 24

---
+++ src/test/scala/level1/TestProblem24.scala
@@ -0,0 +1,22 @@
+package level1
+
+import level1.Problem23.{Abundant, Perfect}
+import level1.Problem24.Permutation
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestProblem24 extends FlatSpec with Matchers {
+
+  "permutationStream" should "give the example" in {
+    Problem24.permutationStream(Seq(0, 1, 2)).toList should be(
+      List(
+        Permutation(List(0, 1, 2)),
+        Permutation(List(0, 2, 1)),
+        Permutation(List(1, 0, 2)),
+        Permutation(List(1, 2, 0)),
+        Permutation(List(2, 0, 1)),
+        Permutation(List(2, 1, 0)))
+    )
+  }
+
+
+}
