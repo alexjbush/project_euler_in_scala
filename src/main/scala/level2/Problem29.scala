ba621432a2dfb2babd72a66abc8aab40a3f30f45
Alex Bush <bushnoh@gmail.com>
2019-07-22 20:53:46 +0100

    Problem 29

---
+++ src/main/scala/level2/Problem29.scala
@@ -0,0 +1,16 @@
+package level2
+
+object Problem29 {
+
+  def numDistinctPowers(maxA: Int, maxB: Int): Int = {
+    for {
+      a <- (2 to maxA).toStream
+      b <- (2 to maxB).toStream
+    } yield BigInt(a).pow(b)
+  }.toSet.size
+
+  def main(args: Array[String]): Unit = {
+    println(s"Answer: ${numDistinctPowers(100, 100)}")
+  }
+
+}
